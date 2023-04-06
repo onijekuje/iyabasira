aade1c0173454cd726b001565b7d25a1482d9d18
mofe <ogunbiyioladapo33@gmail.com>
2023-04-06 12:50:42 +0100

    updates

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-require("dotenv").config({ path: "./config.env" });
-const express = require("express");
-const bodyParser = require("body-parser");
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import MessagingRouter from "./routers/messagingRouter.js";
 
+dotenv.config({ path: "./config.env" });
 const { BOT_TOKEN, lOCAL_SERVER_URL } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
 const URI = `/webhook/${BOT_TOKEN}`;
@@ -9,6 +11,7 @@ const WEBHOOK_URL = lOCAL_SERVER_URL + URI;
 
 const app = express();
 app.use(bodyParser.json());
+app.use(URI, MessagingRouter);
 
 const init = async () => {
   const res = await fetch(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
@@ -16,35 +19,12 @@ const init = async () => {
   console.log(info);
 };
 
-app.post(URI, async (req, res, next) => {
-  console.log(req.body);
-
-  const chatId = req.body.message.chat.id;
-  const text = req.body.message.text;
-  console.log(chatId);
-  console.log(text);
-  try {
-    const x = await fetch(`${TELEGRAM_API}/sendMessage`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        chat_id: chatId,
-        text: `onijekuje way !!!!!! ${text}`,
-      }),
-    });
-    const y = await x.json();
-    console.log("success", y);
-  } catch (error) {
-    console.log(error);
-  }
-
-  res.send();
-});
-
 app.listen(process.env.PORT || 5000, async () => {
   console.log("App running on port", process.env.PORT || 5000);
   await init();
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("unhandled rejection, Shutting down....");
+  console.log(err.name, err.message);
 });
